275e525469fe9c2740fc75a79c188baca7e06f7f
kalun62 <80530886+kalun62@users.noreply.github.com>
2022-09-20 20:08:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -456,7 +456,7 @@ jQuery(document).ready(function() {
 								console.log(iphone_scroll);
 								console.log(iphone_height);
 								setTimeout(() => {
-									$('.active-chat').attr('style',`top: ${iphone_scroll - 85}px !important; height: ${iphone_height * .9 - iphone_scroll}px !important`)
+									$('.active-chat').attr('style',`top: ${iphone_scroll - 85}px !important; height: ${iphone_height - iphone_scroll}px !important`)
 								})
 							}, 500);
 							
