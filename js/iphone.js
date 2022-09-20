3be8f9887ade37254a6a3242c1a6bd8c4d6ba797
kalun62 <80530886+kalun62@users.noreply.github.com>
2022-09-20 14:21:12 +0300

    Update iphone.js

--- js/iphone.js
+++ js/iphone.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function() {	
 
 	$('.bx-im-textarea-input').on('focus', function(e){
-		
+		console.log('фокус')
 		$('.contact-form.bx.active-chat').addClass('openKeyboard')
 	})
 	$('.bx-im-textarea-input').on('blur', function(){
+		console.log('не фокус')
 		$('.contact-form.bx.active-chat').removeClass('openKeyboard')
 	})
 console.log('вроде работает');
