17d6dc7e40a00eae956bf5dd5c2373e5a5fb13ec
kalun62 <80530886+kalun62@users.noreply.github.com>
2022-09-20 14:09:12 +0300

    Update iphone.js

--- js/iphone.js
+++ js/iphone.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function() {
 	$('.bx-im-textarea-input').on('blur', function(){
 		$('.contact-form.bx.active-chat').removeClass('openKeyboard')
 	})
-
+console.log('вроде работает');
 })
 
 // сделать окно на весь экран и поверх всех элементов но в рамках страницы и разрешить скролл всей страницы
