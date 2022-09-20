256efd1bd9f39a6592f7025c00b7adc5ba1476a2
kalun62 <80530886+kalun62@users.noreply.github.com>
2022-09-20 12:22:34 +0300

    Add files via upload

--- js/iphone.js
+++ js/iphone.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function() {	
-$('.contact-form').click((e) => {
-		window.scrollTo(0, 0)
-	})
+
+	// $('.contact-form').click(() => {
+	// 	window.scrollTo(0, 0)
+	// })
+
 })
+
+// сделать окно на весь экран и поверх всех элементов но в рамках страницы и разрешить скролл всей страницы
