b268397fb975786934e567dc7b4d200e695d81f8
kalun62 <80530886+kalun62@users.noreply.github.com>
2022-09-20 14:45:08 +0300

    Update iphone.js

--- js/iphone.js
+++ js/iphone.js
@@ -1,26 +1,16 @@
-// jQuery(document).ready(function() {	
-
-	// $('.contact-form').click(() => {
-	// 	window.scrollTo(0, 0)
-	// })
-	$('.bx-im-textarea-input').on('focus', function(e){
-		// e.preventDefault()
-		$('.contact-form.bx.active-chat').addClass('openKeyboard')
-	})
-	$('.bx-im-textarea-input').on('blur', function(){
-		$('.contact-form.bx.active-chat').removeClass('openKeyboard')
-	})
-
-	console.log('вроде работает');
-// })
 
 // сделать окно на весь экран и поверх всех элементов но в рамках страницы и разрешить скролл всей страницы
-const input = document.querySelector('.bx-im-textarea-input'),
+const contact_form = document.querySelector('.contact-form'),
+	  input = document.querySelector('.bx-im-textarea-input'),
  	  active_chat = document.querySelector('.active-chat')
 
-input.addEventListener('focus', () => {
-	active_chat.classList.add('openKeyboard')
-})
-input.addEventListener('focus', () => {
-	active_chat.classList.remove('openKeyboard')
+contact_form.addEventListener('click', () => {
+	setTimeout(() => {
+		input.addEventListener('focus', () => {
+			active_chat.classList.add('openKeyboard')
+		})
+		input.addEventListener('focus', () => {
+			active_chat.classList.remove('openKeyboard')
+		})
+	}, 100)
 })
