ab43876bc03be8ccf99d0fb490c9bfb82b50f2c1
kalun62 <80530886+kalun62@users.noreply.github.com>
2022-09-20 13:12:03 +0300

    Update iphone.js

--- js/iphone.js
+++ js/iphone.js
@@ -3,9 +3,14 @@ jQuery(document).ready(function() {
 	// $('.contact-form').click(() => {
 	// 	window.scrollTo(0, 0)
 	// })
-	$('.active-chat textarea').on('focus', function(){
+	$('.active-chat textarea').on('focus', function(e){
+		e.preventDefault()
 		$('.active-chat').addClass('openKeyboard')
 	})
+	$('.active-chat textarea').on('blur', function(){
+		$('.active-chat').removeClass('openKeyboard')
+	})
+
 })
 
 // сделать окно на весь экран и поверх всех элементов но в рамках страницы и разрешить скролл всей страницы
