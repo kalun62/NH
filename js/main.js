aaf0922922801a8a265a0c1cb42c09765eb77d3d
kalun62 <80530886+kalun62@users.noreply.github.com>
2022-09-27 21:17:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -414,7 +414,7 @@ jQuery(document).ready(function() {
 								openChat = false
 								$('.active-chat').appendTo('.main-block')
 								$('.chat-wrapper').attr('style', '')
-								
+
 								if(is_ipad){
 									$('.active-chat').attr('style', '')
 									$('.bx-im-textarea-box').append($('<div class="coverTextarea"></div>'))
@@ -478,34 +478,11 @@ jQuery(document).ready(function() {
 							this.style.height = 30 + 'px' 
 							this.style.height = this.scrollHeight + 'px'; 
 							resizeChat()
-						})
-			
-							//---------// SAFARI // -------------
-// ПРОВЕРИТЬ !!! клавиаутра глючит полсе нажатия на textarea !!!
-
-// iphone 13 390/664		- все норм !!! проверил !!! 90%
-// 13 pro	 390/664		- все норм !!! проверил !!! 90%
-// 13 mini 	 375/629 		- все норм !!! проверил !!! 89% можно 90%
-// 12 		 390/664 		- все норм !!! проверил !!!	86%		
-// 12 pro 					- все норм !!! проверил !!! 86%
-// 12 mini   375/629		- все норм !!! проверил !!!	89%	
-// iphone 11(15)414/715 	- все норм !!! проверил !!! 90%
-// iphone 11(14)414/715 	- все норм !!! проверил !!!	87%
-// 11 pro max(14)414/719	- все норм !!! проверил !!!	
-// 11 pro max(16)414/719	- все норм !!! проверил !!! 90%
-// 11pro      				- 
-// iphone 8 				- все норм !!! проверил !!!	88% !! провериить innerheight не сходиться
-// iphone XS(14) 375/635 	- все норм !!! проверил !!! 86%    провериить innerheight не сходиться
-// iphone XS(15) 375/635	- все норм !!! проверил !!!	89%
-// iphone ХR(15)414/719	 	- все норм !!! проверил !!! 90%  
-// SE 2022 (15)	375/548		- все норм !!! проверил !!! 88%   провериить innerheight не сходиться(сделать высоту 88%)
-
-						
-						
+						})	
 					}
-					if(!is_ipad){
+					// if(!is_ipad){
 						$('.bx-im-textarea-input').focus()
-					}
+					// }
 					clearInterval(load)
 				}
 			},100)	
